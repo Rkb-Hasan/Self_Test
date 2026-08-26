5b4bbb609b7cbe4b8465f371c9e3b88fbe61ce99
Rokib <hasanrokibul413@gmail.com>
2026-08-26 23:34:02 +0600

    added funcs

--- Task Board/app.js
+++ Task Board/app.js
@@ -4,8 +4,32 @@ const inProgressList = document.querySelector("#in-progress-list");
 const doneList = document.querySelector("#done-list");
 const clearTasks = document.querySelector("#clear-all-btn");
 const taskLists = document.querySelectorAll(".task-list");
+const board = document.querySelector(".board-container");
 
 const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function populateTask(items) {
+  taskLists.forEach((task) => (task.innerHTML = ""));
+
+  items.map((item) => {
+    const itemList = document.querySelector(`#${item.status}-list`);
+    if (itemList) {
+      const itemHTML = `<li class="task-item" data-id=${item.id} data-status=${item.status}>
+       <p class="task-action">
+          <span>  ${item.text}</span>
+            <button style="${item.status == "done" ? "display: none;" : ""}"  id="update">
+               <img data-id=${item.id} id="update" src="./images/edit.png" alt="edit" />
+            </button>
+            <button   class="danger-btn">
+               <img data-id=${item.id} id="delete" src="./images/delete.png" alt="delete" />
+            </button>
+       </p>
+    </li>`;
+      itemList.innerHTML += itemHTML;
+    }
+  });
+}
+
 function addTask(e) {
   e.preventDefault();
   const text = this.querySelector("input[type=text]").value;
@@ -18,47 +42,68 @@ function addTask(e) {
 
   tasks.push(task);
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  populateTask(tasks, todoList);
+  populateTask(tasks);
 
   this.reset();
 }
 
-function populateTask(items, itemList) {
-  itemList.innerHTML = items
-    .map((item) => {
-      return `
-    <li class="task-item" data-id=${item.id} data-status=${item.status}>
-       <p class="task-action">
-            ${item.text}
-            <button>--></button>
-            <button data-id=${item.id} class="danger-btn">X</button>
-       </p>
-    </li>
-   `;
-    })
-    .join("");
-}
+function deleteTask(el) {
+  console.log("click");
+  const index = tasks.findIndex((task) => task.id === Number(el.dataset.id));
 
-function deleteTask(e) {
-  if (!e.target.matches(".danger-btn")) return;
-  console.log(tasks);
-
-  const index = tasks.findIndex(
-    (task) => task.id === Number(e.target.dataset.id),
-  );
-  console.log(index);
   if (index !== -1) {
     tasks.splice(index, 1);
   }
 
-  populateTask(tasks, todoList);
+  populateTask(tasks);
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+function updateTaskStatus(el) {
+  const status = el.closest(".task-item").dataset.status;
+  const updatedStatus = status === "todo" ? "in-progress" : "done";
+
+  const newTasks = tasks.map((task) => {
+    if (!(task.id === Number(el.dataset.id))) {
+      return task;
+    }
+    task.status = updatedStatus;
+    return task;
+  });
+
+  tasks.splice(0, tasks.length, ...newTasks);
+  populateTask(tasks);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function action(e) {
+  const el = e.target;
+  console.log(el);
+
+  if (el.matches("#delete")) {
+    console.log("yes");
+    deleteTask(el);
+    // console.log(el);
+  } else if (el.matches("#update")) {
+    updateTaskStatus(el);
+  }
+}
+
+function clearAllTasks() {
+  const currentStore = JSON.parse(localStorage.getItem("tasks"));
+  if (!(currentStore?.length && currentStore !== null)) {
+    alert("Task board is empty!");
+    return;
+  }
+
+  const confirmed = confirm("Are you sure?");
+  if (confirmed) {
+    populateTask([]);
+    localStorage.setItem("tasks", JSON.stringify([]));
+  }
+}
+
+board.addEventListener("click", action);
 addTasks.addEventListener("submit", addTask);
-
-taskLists.forEach((taskList) => {
-  taskList.addEventListener("click", deleteTask);
-});
-
-populateTask(tasks, todoList);
+clearTasks.addEventListener("click", clearAllTasks);
+populateTask(tasks);
